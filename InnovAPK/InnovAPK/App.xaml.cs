2d3cb08ce66200769db6e5159091fd16bbd8ffed
kayembajoel92@gmail.com <kayembajoel92@gmail.com>
2024-06-01 22:10:50 -0400

    Ajout de la page publication

--- InnovAPK/InnovAPK/App.xaml.cs
+++ InnovAPK/InnovAPK/App.xaml.cs
@@ -11,7 +11,7 @@ namespace InnovAPK
         {
             InitializeComponent();
 
-            MainPage = new ConnexionPage();
+            MainPage = new PublicationPage();
         }
 
         protected override void OnStart()
